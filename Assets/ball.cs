470e8884797fd486c5d9d5099f4e91b747a38c3c
VishnuD382 <vishnudhanasekaran9@gmail.com>
2020-10-02 14:26:43 -0400

    fixed colliders

---
+++ Assets/ball.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public float maxSpeed = 0.5f;//Replace with your max speed
+    void FixedUpdate()
+    {
+        if (GetComponent<Rigidbody>().velocity.magnitude > maxSpeed)
+        {
+            GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity.normalized * maxSpeed;
+        }
+    }
+}
--- Assets/tilt.cs
+++ Assets/tilt.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class tilt : MonoBehaviour
 {
-    public float rotateSpeed = 1;
+    public float rotateSpeed = 0.5f;
 
-    
+    public Vector3 currentRotation;
 
     // Start is called before the first frame update
     void Start()
@@ -17,24 +17,27 @@ public class tilt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxis("Horizontal") > .2)
+
+        currentRotation = GetComponent<Transform>().eulerAngles;
+
+        if(Input.GetAxis("Horizontal") > .2  && (currentRotation.z <= 10 || currentRotation.z > 348)   )
         {
             transform.Rotate(0, 0, rotateSpeed);
         }
 
-        if (Input.GetAxis("Horizontal") <  -.2)
+        if (Input.GetAxis("Horizontal") <  -.2  && (currentRotation.z >= 349 || currentRotation.z <= 11))
         {
             transform.Rotate(0, 0, -rotateSpeed);
         }
 
 
-        if (Input.GetAxis("Vertical") > .2)
+        if (Input.GetAxis("Vertical") > .2   && (currentRotation.x <= 10 || currentRotation.x >= 348))
         {
             transform.Rotate(rotateSpeed, 0, 0);
         }
 
 
-        if (Input.GetAxis("Vertical") < -.2)
+        if (Input.GetAxis("Vertical") < -.2  && (currentRotation.x >= 349 || currentRotation.x <= 11))
         {
             transform.Rotate(-rotateSpeed, 0, 0);
         }
